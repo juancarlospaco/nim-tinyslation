f0f8ab9cc0a7655a97f08f18314dc1cbd881bb67
juancarlospaco <juancarlospaco@gmail.com>
2018-05-02 20:07:47 -0300

    Init.

---
+++ src/translation.nim
@@ -0,0 +1,15 @@
+import httpclient, json, strformat, uri, pylib
+
+
+proc tinyslation*(text: string, to: string, frm: string="en", fallback_value: string=""): string =
+    ## Text string translation from free online crowdsourced API.
+    assert frm != to and len(text) > 2 and len(frm) == 2 and len(to) == 2
+    let ul = fmt"http://mymemory.translated.net/api/get?q={encodeUrl(text)}&langpair={frm}|{to}"
+    try:
+        result = $parse_json(get(ul).body)["responseData"]["translatedText"]
+    except Exception:
+        result = if fallback_value: fallback_value else: text
+
+
+# if is_main_module:
+#     echo tinyslation("white cat", to="es")  # "gato blanco"
---
+++ translation.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Juan Carlos <juancarlospaco@gmail.com>"
+description   = "Text string translation from free online crowdsourced API. Tinyslation a tiny translation."
+license       = "LGPLv3"
+srcDir        = "src"
+bin           = @["translation.nim"]
+
+# Dependencies
+
+requires "nim >= 0.18.1"
+requires "pylib"
