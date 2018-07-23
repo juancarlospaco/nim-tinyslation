f420f8c9e950dc1c5c211acc3ca64bbfb4bd6b14
juancarlospaco <juancarlospaco@gmail.com>
2018-07-23 01:05:58 -0300

    Add Async and Sync support using MultiSync.

--- src/translation.nim
+++ src/translation.nim
@@ -1,14 +1,27 @@
-import httpclient, json, strformat, uri, pylib
+import asyncdispatch, httpclient, json, strformat, uri
 
-proc tinyslation*(text: string, to: string, frm: string="en", fallback_value: string=""): string =
-    ## Text string translation from free online crowdsourced API.
-    assert frm != to and len(text) > 2 and len(frm) == 2 and len(to) == 2
-    let ul = fmt"http://mymemory.translated.net/api/get?q={encodeUrl(text)}&langpair={frm}|{to}"
-    try:
-        result = $parse_json(get(ul).body)["responseData"]["translatedText"]
-    except Exception:
-        result = if fallback_value: fallback_value else: text
+const base_url =
+  when defined(ssl): "https://mymemory.translated.net/api/get?q="  ## SSL.
+  else:              "http://mymemory.translated.net/api/get?q="   ## No SSL.
+type
+  MMT* = HttpClient            ## Sync Client for translations.
+  AsyncMMT* = AsyncHttpClient  ## Async Client for translations.
 
 
-# if is_main_module:
-#     echo tinyslation("white cat", to="es")  # "gato blanco"
+proc tinyslation*(this: MMT | AsyncMMT, text: string, to: string, frm="en", timeout: int8= 9): Future[string] {.multisync.} =
+  ## Text string translation from free online crowdsourced API. Sync and Async.
+  assert frm != to and len(text) > 2 and len(frm) == 2 and len(to) == 2, "Tinyslation Wrong Arguments."
+  let
+    url = fmt"{base_url}{encodeUrl(text)}&langpair={frm}|{to}"
+    responz =
+      when this is AsyncMMT: await newAsyncHttpClient().get(url)
+      else: newHttpClient(timeout=timeout * 1000).get(url)
+  result = $parse_json(await responz.body)["responseData"]["translatedText"]
+
+
+when is_main_module:
+  echo MMT().tinyslation("white cat", to="es")
+
+  proc async_translation {.async.} = echo await AsyncMMT().tinyslation("white cat", to="es")
+
+  wait_for async_translation()
--- translation.nimble
+++ translation.nimble
@@ -2,12 +2,11 @@
 
 version       = "0.1.0"
 author        = "Juan Carlos <juancarlospaco@gmail.com>"
-description   = "Text string translation from free online crowdsourced API. Tinyslation a tiny translation."
+description   = "Text string translation from free online crowdsourced API. Tinyslation a tiny translation. Sync and Async support (MultiSync). No API Key required."
 license       = "LGPLv3"
 srcDir        = "src"
-bin           = @["translation.nim"]
+
 
 # Dependencies
 
 requires "nim >= 0.18.0"
-requires "pylib"
